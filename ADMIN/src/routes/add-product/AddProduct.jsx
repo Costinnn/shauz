9be89e6409c5b8c028829a8fbcafb75742ab78e9
Costinnn <crauciuc.costin@yahoo.com>
2023-04-20 16:29:35 +0300

    admin add product to db

--- ADMIN/src/routes/add-product/AddProduct.jsx
+++ ADMIN/src/routes/add-product/AddProduct.jsx
@@ -1,7 +1,248 @@
-import './AddProduct.scss'
+import { useState } from "react";
+import axios from "axios";
+
+import "./AddProduct.scss";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [category, setCategory] = useState("");
+  const [categoryList, setCategoryList] = useState([]);
+  const [price, setPrice] = useState("");
+  const [atSale, setAtSale] = useState(false);
+  const [oldPrice, setOldPrice] = useState("");
+  const [stock, setStock] = useState({ xs: 0, s: 0, m: 0, l: 0, xl: 0 });
+  const [image, setImage] = useState("");
+  const [imageList, setImageList] = useState([]);
+
+  const handleCategoryList = () => {
+    if (category) {
+      setCategoryList((prev) => [...prev, category]);
+      setCategory("");
+    }
+  };
+  const handleStock = (size, value) => {
+    setStock((prev) => ({ ...prev, [size]: value }));
+  };
+
+  const handleImageList = () => {
+    if (image) {
+      setImageList((prev) => [...prev, image]);
+      setImage("");
+    }
+  };
+
+  const addProductToDb = async (newProduct) => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_POST_LOCAL_URL,
+        newProduct
+      );
+      if (response) {
+        console.log(response.data);
+        console.log("Product added with success!");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newProduct = {
+      title,
+      desc,
+      images: imageList,
+      stockQ: stock,
+      category: categoryList,
+      price: Number(price),
+      oldPrice: Number(oldPrice),
+      sale: atSale,
+    };
+
+    addProductToDb(newProduct);
+
+    setTitle("");
+    setDesc("");
+    setCategory("");
+    setCategoryList([]);
+    setPrice("");
+    setAtSale(false);
+    setOldPrice("");
+    setStock({ xs: 0, s: 0, m: 0, l: 0, xl: 0 });
+    setImage("");
+    setImageList([]);
+  };
+
+  return (
+    <main className="section-narrow addproduct">
+      <h1>Add new product</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="title">
+          <label htmlFor="title">Title:</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            name="title"
+            id="title"
+            required
+          />
+        </div>
+
+        <div className="desc">
+          <label htmlFor="desc">Description:</label>
+          <textarea
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            name="desc"
+            id="desc"
+            cols="60"
+            rows="5"
+            required
+          />
+        </div>
+
+        <div className="category">
+          <label htmlFor="category">Category:</label>
+          <input
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            type="text"
+            name="category"
+            id="category"
+          />
+          <span className="button1" onClick={handleCategoryList}>
+            Add
+          </span>
+          <br />
+          <span>{categoryList && categoryList.map((item) => `${item}, `)}</span>
+        </div>
+
+        <div className="price">
+          <label htmlFor="price">Price:</label>
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            type="number"
+            name="price"
+            id="price"
+            required
+          />
+        </div>
+
+        <div className="sale">
+          <label htmlFor="sale">Sale:</label>
+          <input
+            value={oldPrice}
+            onChange={(e) => setOldPrice(e.target.value)}
+            type="number"
+            name="sale"
+            id="sale"
+            placeholder="Old price"
+          />
+          <input
+            checked={atSale}
+            onChange={() => setAtSale(!atSale)}
+            type="checkbox"
+            name="yes"
+            id="yes"
+            value="true"
+          />
+          <label htmlFor="yes">Yes</label>
+          <input
+            checked={!atSale}
+            onChange={() => setAtSale(!atSale)}
+            type="checkbox"
+            name="no"
+            id="no"
+            value="false"
+          />
+          <label htmlFor="no">No</label>
+        </div>
+
+        <div className="stock">
+          <label htmlFor="stock">Stock:</label>
+
+          <span>XS</span>
+          <input
+            value={stock.xs}
+            onChange={(e) => handleStock("xs", e.target.value)}
+            type="number"
+            name="stock"
+            id="stock"
+            min="0"
+            required
+          />
+
+          <span>S</span>
+          <input
+            value={stock.s}
+            onChange={(e) => handleStock("s", e.target.value)}
+            type="number"
+            name="stock"
+            id="stock"
+            min="0"
+            required
+          />
+
+          <span>M</span>
+          <input
+            value={stock.m}
+            onChange={(e) => handleStock("m", e.target.value)}
+            type="number"
+            name="stock"
+            id="stock"
+            min="0"
+            required
+          />
+
+          <span>L</span>
+          <input
+            value={stock.l}
+            onChange={(e) => handleStock("l", e.target.value)}
+            type="number"
+            name="stock"
+            id="stock"
+            min="0"
+            required
+          />
+
+          <span>XL</span>
+          <input
+            value={stock.xl}
+            onChange={(e) => handleStock("xl", e.target.value)}
+            type="number"
+            name="stock"
+            id="stock"
+            min="0"
+            required
+          />
+        </div>
+
+        <div className="images">
+          <label htmlFor="img">Images:</label>
+          <input
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            type="text"
+            name="img"
+            id="img"
+            placeholder="Image link"
+          />
+          <span className="button1" onClick={handleImageList}>
+            Add
+          </span>
+
+          <div className="img-added">
+            {imageList && imageList.map((img) => <span key={img}>{img}</span>)}
+          </div>
+        </div>
+        <button className="button3">ADD PRODUCT</button>
+      </form>
+    </main>
+  );
 };
 
 export default AddProduct;
--- ADMIN/src/routes/product/Product.jsx
+++ ADMIN/src/routes/product/Product.jsx
@@ -10,6 +10,7 @@ const Product = () => {
   return (
     <main className="section-narrow product-page">
       <h1>{product.title}</h1>
+      <span>Product ID: {product._id}</span>
       <div className="photos">
         {product.images.map((photo) => (
           <img src={photo} alt="" key={photo} />
@@ -59,8 +60,64 @@ const Product = () => {
         </div>
       </div>
       <div className="update-stock">
-        <h3>Stock</h3>
+        <h3>Stock:</h3>
+        <div>
+          <div className="stock-item">
+            <span>XS: {product.stockQ.xs}</span>
+            <input type="number" name="" id="" min="0" />
+            <button className="button1">Update</button>
+          </div>
+          <div className="stock-item">
+            <span>S: {product.stockQ.s}</span>
+            <input type="number" name="" id="" min="0" />
+            <button className="button1">Update</button>
+          </div>
+          <div className="stock-item">
+            <span>M: {product.stockQ.m}</span>
+            <input type="number" name="" id="" min="0" />
+            <button className="button1">Update</button>
+          </div>
+          <div className="stock-item">
+            <span>L: {product.stockQ.l}</span>
+            <input type="number" name="" id="" min="0" />
+            <button className="button1">Update</button>
+          </div>
+          <div className="stock-item">
+            <span>XL: {product.stockQ.xl}</span>
+            <input type="number" name="" id="" min="0" />
+            <button className="button1">Update</button>
+          </div>
+        </div>
       </div>
+      <div className="update-category">
+        <h3>Category:</h3>
+        <div>
+          {product.category.map((item) => (
+            <div className="category-item" key={item}>
+              <span>{item}</span>
+              <button className="button2">Delete</button>
+            </div>
+          ))}
+          <div>
+            <input type="text" name="" id="" placeholder="New category" />
+            <button className="button1">Add</button>
+          </div>
+        </div>
+      </div>
+      <div className="update-general">
+        <div className="price">
+          <h3>Price:</h3>
+          <span>{product.price} lei </span>
+          <span>
+            {product.oldPrice ? ` (redus de la ${product.oldPrice} lei)` : ""}
+          </span>
+        </div>
+        <div className="sale">
+          <h3>Sale:</h3>
+          <span>{product.sale ? "YES" : "NO"}</span>
+        </div>
+      </div>
+      <button className="button4">DELETE PRODUCT</button>
     </main>
   );
 };
